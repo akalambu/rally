468ea0e0d867c9b6c093e95603783d1dc598379b
Ajay Kalambur <akalambu@cisco.com>
2015-06-05 21:46:42 -0700

    Changes for context and cleanup to enable scale

--- rally/plugins/openstack/scenarios/gbp/utils.py
+++ rally/plugins/openstack/scenarios/gbp/utils.py
@@ -3,8 +3,8 @@ from rally import osclients
 import os
 import time
 
-@osclients.Clients.register("gbp")
-def gbp(self):
+@osclients.Clients.register("grouppolicy")
+def grouppolicy(self):
 	from gbpclient.v2_0 import client as gbpclient
 	return gbpclient.Client(username=self.endpoint.username,
 							password=self.endpoint.password,
@@ -23,7 +23,7 @@ class GBPScenario(base.Scenario):
 			"action_type": type
 			}
 		}
-		self.clients("gbp").create_policy_action(body)
+		self.clients("grouppolicy").create_policy_action(body)
 
 	@base.atomic_action_timer("gbp.delete_policy_action")
 	def _delete_policy_action(self, name="allow"):
@@ -33,7 +33,7 @@ class GBPScenario(base.Scenario):
 		"""
 		policy_id = self._find_policy_actions(name)
 		if policy_id:
-			self.clients("gbp").delete_policy_action(policy_id)
+			self.clients("grouppolicy").delete_policy_action(policy_id)
 			return
 		print "Policy action %s not found" %(name)
 		return
@@ -44,7 +44,7 @@ class GBPScenario(base.Scenario):
 		Lookup 10 second interval
 		"""
 		for i in range(10):
-			actions = self.clients("gbp").list_policy_actions()
+			actions = self.clients("grouppolicy").list_policy_actions()
 			for policy in actions['policy_actions']:
 				if policy['name'] == name:
 					return policy['id']
@@ -61,13 +61,13 @@ class GBPScenario(base.Scenario):
 				"direction": direction
 			}
 		}
-		self.clients("gbp").create_policy_classifier(body)
+		self.clients("grouppolicy").create_policy_classifier(body)
 
 	@base.atomic_action_timer("gbp.delete_policy_classifier")
 	def _delete_policy_classifier(self, name):
 		classifier_id = self._find_policy_classifier(name)
 		if classifier_id:
-			self.clients("gbp").delete_policy_classifier(classifier_id)
+			self.clients("grouppolicy").delete_policy_classifier(classifier_id)
 			return
 		print "Policy classifier %s is not found" %(name)
 		return
@@ -77,7 +77,7 @@ class GBPScenario(base.Scenario):
 		Find a policy classifier given its name
 		"""
 		for i in range(10):
-			classifiers = self.clients("gbp").list_policy_classifiers()
+			classifiers = self.clients("grouppolicy").list_policy_classifiers()
 			for classifier in classifiers["policy_classifiers"]:
 				if classifier['name'] == name:
 					return classifier['id']
@@ -93,14 +93,14 @@ class GBPScenario(base.Scenario):
 				"name": policy_name
 			}
 		}
-		self.clients("gbp").create_policy_rule(body)
+		self.clients("grouppolicy").create_policy_rule(body)
 
 	def _find_policy_rule(self, name):
 		"""
 		Find a policy rule given its name
 		"""
 		for i in range(10):
-			rules = self.clients("gbp").list_policy_rules()
+			rules = self.clients("grouppolicy").list_policy_rules()
 			for rule in rules["policy_rules"]:
 				if rule['name'] == name:
 					return rule['id']
@@ -111,7 +111,7 @@ class GBPScenario(base.Scenario):
 	def _delete_policy_rule(self, name):
 		policy_rule_id = self._find_policy_rule(name)
 		if policy_rule_id:
-			self.clients("gbp").delete_policy_rule(policy_rule_id)
+			self.clients("grouppolicy").delete_policy_rule(policy_rule_id)
 			return
 		print "Policy rule %s not found" %(name)
 		return
@@ -129,14 +129,14 @@ class GBPScenario(base.Scenario):
 				"policy_rules": ruleid_list
 			}
 		}
-		self.clients("gbp").create_policy_rule_set(body)
+		self.clients("grouppolicy").create_policy_rule_set(body)
 	
 	def _find_policy_rule_set(self, name):
 		"""
 		Find a policy rule set given its name
 		"""
 		for i in range(10):
-			rule_set = self.clients("gbp").list_policy_rule_sets()
+			rule_set = self.clients("grouppolicy").list_policy_rule_sets()
 			for ruleset in rule_set["policy_rule_sets"]:
 				if ruleset['name'] == name:
 					return ruleset['id']
@@ -147,7 +147,7 @@ class GBPScenario(base.Scenario):
 	def _delete_policy_rule_set(self, name):
 		policy_ruleset_id = self._find_policy_rule_set(name)
 		if policy_ruleset_id:
-			self.clients("gbp").delete_policy_rule_set(policy_ruleset_id)
+			self.clients("grouppolicy").delete_policy_rule_set(policy_ruleset_id)
 			return
 		print "Policy rule set %s not found" %(name)
 		return
@@ -159,14 +159,14 @@ class GBPScenario(base.Scenario):
 				"name": name
 			}
 		}
-		self.clients("gbp").create_policy_target_group(body)
+		self.clients("grouppolicy").create_policy_target_group(body)
 		
 	def _find_policy_target_group(self, name):
 		"""
 		Find a policy target group given the name
 		"""
 		for i in range(10):
-			groups = self.clients("gbp").list_policy_target_groups()
+			groups = self.clients("grouppolicy").list_policy_target_groups()
 			for group in groups["policy_target_groups"]:
 				if group['name'] == name:
 					return group['id']
@@ -177,7 +177,7 @@ class GBPScenario(base.Scenario):
 	def _delete_policy_target_group(self, name):
 		group_id = self._find_policy_target_group(name)
 		if group_id:
-			self.clients("gbp").delete_policy_target_group(group_id)
+			self.clients("grouppolicy").delete_policy_target_group(group_id)
 			return
 		print "Policy target group %s not found" %(name)
 		return
@@ -203,7 +203,7 @@ class GBPScenario(base.Scenario):
 				"consumed_policy_rule_sets" : consumed_dict
 			}
 		}
-		self.clients("gbp").update_policy_target_group(group_id, body)
+		self.clients("grouppolicy").update_policy_target_group(group_id, body)
 	
 	@base.atomic_action_timer("gbp.create_policy_target")
 	def _create_policy_target(self, name, group_name):
@@ -215,11 +215,11 @@ class GBPScenario(base.Scenario):
 				"name": name
 			}
 		}
-		self.clients("gbp").create_policy_target(body)
+		self.clients("grouppolicy").create_policy_target(body)
 	
 	def _find_policy_target(self, name):
 		for i in range(10):
-			targets = self.clients("gbp").list_policy_targets()
+			targets = self.clients("grouppolicy").list_policy_targets()
 			for target in targets["policy_targets"]:
 				if target['name'] == name:
 					return target['id']
@@ -230,7 +230,7 @@ class GBPScenario(base.Scenario):
 	def _delete_policy_target(self, name):
 		target_id = self._find_policy_target(name)
 		if target_id:
-			self.clients("gbp").delete_policy_target(target_id)
+			self.clients("grouppolicy").delete_policy_target(target_id)
 			return
 		print "Policy target %s not found" %(name)
 		return
